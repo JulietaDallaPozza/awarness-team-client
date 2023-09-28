eb710adb5b6b0c9c624a9172e27b5398e765b2f6
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-09-28 10:10:06 +0200

    wip

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
+import { AuthProviderWrapper } from "./context/auth.context";
+import { BrowserRouter as Router } from "react-router-dom";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { AuthProviderWrapper } from "./context/auth.context";
-import { BrowserRouter as Router } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
